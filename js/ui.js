d4b2c2411e89f30433423e00e1c03bd6c278db11
akl15 <allanleek@gmail.com>
2014-02-27 20:26:25 -0800

    added some jquery to make the dropdown menu hidden on page load.

--- js/ui.js
+++ js/ui.js
@@ -1,12 +1,14 @@
 $(document).ready( function() {
 
 
-
+// THIS GIVES THE DROPDOWN MENU A SMOOTH TRANSITION
 	$("#navToggle").click( function() {
 		$("nav").slideToggle(1000);
 
 	});
 
+
+// THIS MAKES THE NAVIGATION TURN INTO A DROPDOWN MENU ON RESIZE
 	$(window).resize( function() {
 		var windowWidth = $(window).width();
 		if (windowWidth>885) {
@@ -17,6 +19,20 @@ $(document).ready( function() {
 			$("#navToggle").css("display","block");
 			$("nav").hide();
 		}
-	})
+	});
+
+
+// THIS MAKES THE NAVIGATION TURN INTO A DROWPDOWN MENU ON PAGE LOAD
+	$(document).ready( function() {
+		var windowWidth = $(window).width();
+		if (windowWidth>885) {
+			$("#navToggle").css("display","none");
+			$("nav").show();
+		}
+		else {
+			$("#navToggle").css("display","block");
+			$("nav").hide();
+		}
+	});
 
 });
