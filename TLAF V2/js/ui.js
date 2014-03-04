0963d990c70bc7d05403cb5c35beea33a22d8b1d
akl15 <allanleek@gmail.com>
2014-03-04 12:57:45 -0800

    jquery for v2

---
+++ TLAF V2/js/ui.js
@@ -0,0 +1,38 @@
+$(document).ready( function() {
+
+
+// THIS GIVES THE DROPDOWN MENU A SMOOTH TRANSITION
+	$("#navToggle").click( function() {
+		$("nav").slideToggle(1000);
+
+	});
+
+
+// THIS MAKES THE NAVIGATION TURN INTO A DROPDOWN MENU ON RESIZE
+	$(window).resize( function() {
+		var windowWidth = $(window).width();
+		if (windowWidth>985) {
+			$("#navToggle").css("display","none");
+			$("nav").show();
+		}
+		else {
+			$("#navToggle").css("display","block");
+			$("nav").hide();
+		}
+	});
+
+
+// THIS MAKES THE NAVIGATION TURN INTO A DROWPDOWN MENU ON PAGE LOAD
+	$(document).ready( function() {
+		var windowWidth = $(window).width();
+		if (windowWidth>985) {
+			$("#navToggle").css("display","none");
+			$("nav").show();
+		}
+		else {
+			$("#navToggle").css("display","block");
+			$("nav").hide();
+		}
+	});
+
+});
